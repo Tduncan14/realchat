eecd3f7ad67111b057dc4ed7d8203a8667020194
tduncan14 <nbatre1@yahoo.com>
2026-04-25 21:30:08 -0500

    keep moving forward

--- backend/Controllers/auth.controllers.js
+++ backend/Controllers/auth.controllers.js
@@ -1,24 +1,63 @@
+import User from "../models/user.models"
+
 export const loginUser = async (req, res) => {
 
+
+
+}
+
+
+export const logout = async (req, res) => {
+
+    res.send('this logout sessions')
+}
+
+export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body
 
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Password don't match" })
+        }
+
+        const user = await User.findOne({ username })
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" })
+        }
+
+        // Hassh pasword her
+
+        // this is the picture
+
+        const boyProfilePic = "oriojeirjew"
+        const girlProfilePic = "iargehrgajjra"
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+
+        await newUser.save()
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        })
 
     }
 
     catch (err) {
 
+        console.log("error in signup Controller")
+        res.status(500).json({ error: "Internal Server Error" })
+
 
     }
-
-}
-
-
-export const logout = (req, res) => {
-
-    res.send('this logout sessions')
-}
-
-export const signup = (req, res) => {
-    res.send('this is the signup')
 }
--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ import connectToMongoDb from "./DB/connectToMongoDB.js"
 const app = express()
 dotenv.config()
 
-const PORT = process.env.PORT || 5000;
 
 
-
-
-
+app.use(express.json())
 app.use("/api/auth", authRoutes)
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
