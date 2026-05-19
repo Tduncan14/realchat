91f782cfb8c9684323877491278976d2c4cebc93
tduncan14 <nbatre1@yahoo.com>
2026-05-18 21:35:48 -0500

    keep moving forward

--- backend/Controllers/auth.controllers.js
+++ backend/Controllers/auth.controllers.js
@@ -5,6 +5,35 @@ import { generateTokenAndSetCookie } from "../utils/generateToken.js"
 export const loginUser = async (req, res) => {
 
 
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username })
+
+
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid Username or password" })
+        }
+
+        generateTokenAndSetCookie(user._id, res)
+
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic
+        })
+
+    }
+
+    catch (err) {
+        console.log("Error in the signup controller", error.message)
+        res.status(500).json({ error: "Internal Server ERROR" })
+    }
+
 
 }
 
