7f60de819ad32aae7ce3298f151159326eacebc3
tduncan14 <nbatre1@yahoo.com>
2026-05-23 21:40:59 -0500

    keep moving forward

--- backend/Controllers/message.controller.js
+++ backend/Controllers/message.controller.js
@@ -1,11 +1,41 @@
+import Conversation from '../models/conversation.model.js'
+import Message from '../models/message.model.js'
+
 
 export const sendMessage = async (req, res) => {
 
     try {
         const { message } = req.body;
-        const { id } = req.params
+        const { id: receiverId } = req.params
+        const senderId = req.user._id
 
-        const senderId = req.userId
+
+        let conversation = await Conversation.findOne({
+            participants: {
+                $all: [senderId, receiverId]
+            }
+        })
+
+
+        if (!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId]
+            })
+        }
+
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message
+        })
+
+        if (newMessage) {
+            conversation.message.push(newMessage._id)
+        }
+
+
+        res.status(201).json(newMessage)
 
     }
 
